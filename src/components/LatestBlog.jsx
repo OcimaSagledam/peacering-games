e86f6aafca8ca5a4428def6864fcd2419dd09100
OcimaSagledam <justcurlygames@gmail.com>
2022-09-06 01:05:39 +0200

    Responsivity Update
    Small update to responsivity on smaller devices

--- src/components/LatestBlog.jsx
+++ src/components/LatestBlog.jsx
@@ -2,7 +2,7 @@ export default function LatestBlog({ url, title, description }) {
 
     return <div>
         <div className="flex flex-col xl:flex-row h-full mx-4 xl:mx-80 items-center">
-            <img src={url} className="w-[330] h-[213px] m-1 p-1 border border-black" alt="" />
+            <img src={url} className="w-[330px] h-[213px] m-1 p-1 border border-black" alt="" />
             <div className="px-2 mx-auto flex flex-col h-full">
                 <h1 className="text-3xl font-bold text-center mb-2 xl:mb-8">{title}.</h1>
                 <p className="w-2/3 mx-auto text-lg">{description}</p>
--- src/components/LatestItem.jsx
+++ src/components/LatestItem.jsx
@@ -4,9 +4,8 @@ export default function LatestItem({ urlHP, name, description}) {
     <div className="flex flex-col xl:flex-row h-full px-4 xl:px-80 items-center">         
         <img src={urlHP} className="w-[305px] h-[396px] m-1 p-1 border border-black" alt="" />
         <div className="p-2 mx-auto h-full flex flex-col">
-            <h1 className="text-3xl font-bold text-center mb-2 xl:mb-8">{name}</h1>
-            <p className="w-2/3 mx-auto text-lg">{description}</p>
-
+            <h1 className="text-l xl:text-3xl font-bold text-center mb-2 xl:mb-8">{name}</h1>
+            <p className="mx-auto text-xs sm:text-base xl:text-lg">{description}</p>
             <div className="h-full flex items-center xl:items-end justify-center">
                 <button className='w-fit p-4 mt-2 text-3xl rounded-full transition bg-[#FF6161] hover:bg-[#ff4f4f] hover:scale-105'>PRE-ORDER NOW</button>
             </div>
